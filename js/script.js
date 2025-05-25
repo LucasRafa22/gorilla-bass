98efbecda5a18aa4621043d8361ad6b19d29dd7b
LucasRafa22 <lucassolimene22@gmail.com>
2025-05-25 19:25:33 -0300

    Função resetarJogo adiocionada e configurada

--- js/script.js
+++ js/script.js
@@ -2,10 +2,16 @@ let vidaGorila = 100;
 let humanosRestantes = 100;
 let ataques = 0
 
-
-
 function log(texto) {
     const logDiv = document.getElementById("log");
     logDiv.innerHTML += `<p>${texto}</p>`;
     logDiv.scrollTop = logDiv.scrollHeight;
+}
+
+function resetarJogo(){
+    vidaGorila = 100;
+    humanosRestantes = 100;
+    ataques = 0;
+    localStorage.clear();
+    log("Novo jogo iniciado.");
 }
